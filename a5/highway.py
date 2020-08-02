37d45c4f645917d4bfff62ab7afeef620bdbc54c
Matt Paden <mjpaden@gmail.com>
2020-08-02 10:40:35 -0700

    Correct activation functions

--- a5/highway.py
+++ a5/highway.py
@@ -8,21 +8,16 @@ CS224N 2019-20: Homework 5
 import torch
 import torch.nn as nn
 
+
 class Highway(nn.Module):
-    # Remember to delete the above 'pass' after your implementation
-    ### YOUR CODE HERE for part 1f
 
     def __init__(self, dim: int):
         super(Highway, self).__init__()
-        self.gate = nn.Linear(dim, dim)
-        self.relu = nn.ReLU()
         self.proj = nn.Linear(dim, dim)
-        self.sigmoid = nn.Sigmoid()
+        self.gate = nn.Linear(dim, dim)
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        x_gate = self.relu(self.gate(input))
-        x_proj = self.sigmoid(self.proj(input))
+        x_proj = torch.relu(self.proj(input))
+        x_gate = torch.sigmoid(self.gate(input))
         output = x_gate * x_proj + (1 - x_gate) * input
         return output
-
-    ### END YOUR CODE
